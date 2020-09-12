dee9346e7de1049fa244baabd0212e80077ae2e2
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-09-12 15:44:02 -0300

    Adding Tests

--- PersonTests/test/Library.Test/PersonTests.cs
+++ PersonTests/test/Library.Test/PersonTests.cs
@@ -17,32 +17,94 @@ namespace Tests
             Person testingPerson = new Person("name","5.342.187-9");
             string expectedName = "name";
             string actualName = testingPerson.Name;
-            Assert.AreEqual(expectedName, testingPerson);
+            Assert.AreEqual(expectedName, actualName);
         }
 
+        [Test]
           public void IncorrectNameVerifyer() // Cambiá el nombre para indicar qué estás probando
         {
             Person testingPerson = new Person("","5.342.187-9");
-            string expectedName = "";
+            string expectedName = null;
             string actualName = testingPerson.Name;
-            Assert.AreEqual(expectedName, testingPerson);
+            Assert.AreEqual(expectedName, actualName);
         }
 
+
+        [Test]
          public void CorrectIdVerifyer() // Cambiá el nombre para indicar qué estás probando
         {
-            Person testingPerson = new Person("","5.342.187-9");
-            string expectedName = "5.342.187-9";
-            string actualName = testingPerson.Name;
-            Assert.AreEqual(expectedName, testingPerson);
+            Person testingPerson = new Person("Gonzalo","5.342.187-9");
+            string expectedId = "5.342.187-9";
+            string actualId = testingPerson.ID;
+            Assert.AreEqual(expectedId, actualId);
         }
 
-         public void IncorrectIdVerifyer() // Cambiá el nombre para indicar qué estás probando
+        [Test]
+         public void IncorrectIdVerifyer() 
         {
             Person testingPerson = new Person("","5.342.187-8");
-            string expectedName = "";
-            string actualName = testingPerson.Name;
-            Assert.AreEqual(expectedName, testingPerson);
+            string expectedId = null;
+            string actualId = testingPerson.ID;
+            Assert.AreEqual(expectedId, actualId);
         }
+
+        [Test]
+        public void ChangeToCorrectName() // Cambiá el nombre para indicar qué estás probando
+        {
+            Person testingPerson = new Person("name","5.342.187-9");
+            testingPerson.Name =  "newCorrectName";
+            string expectedName = "newCorrectName";
+            string actualName = testingPerson.Name;
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+          public void ChangeToIncorrectName() 
+        {
+            Person testingPerson = new Person("name","5.342.187-9");
+            testingPerson.Name =  "";
+            string expectedName = "name";
+            string actualName = testingPerson.Name;
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+          public void ChangeToCorrectId() 
+        {
+            Person testingPerson = new Person("name","6.000.000-8");
+            testingPerson.ID =  "5.900.000-9";
+            string expectedId = "5.900.000-9";
+            string actualId = testingPerson.ID;
+            Assert.AreEqual(expectedId, actualId);
+        }
+
+        [Test]
+          public void ChangeToInorrectId() 
+        {
+            Person testingPerson = new Person("name","6.000.000-8");
+            testingPerson.ID =  "5.900.000-1";
+            string expectedId = "6.000.000-8";
+            string actualId = testingPerson.ID;
+            Assert.AreEqual(expectedId, actualId);
+        }
+
+        [Test]
+          public void ChangeToNullId() 
+        {
+            Person testingPerson = new Person("name","6.000.000-8");
+            testingPerson.ID =  "";
+            string expectedId = "6.000.000-8";
+            string actualId = testingPerson.ID;
+            Assert.AreEqual(expectedId, actualId);
+        }
+
+        
+
+
+
+
+
+
 
     }
 }
